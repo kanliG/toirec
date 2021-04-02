f6e04dd280c2c8efe3bb33e89d70d216a18ca90f
Georgia Kanli <kanli.g@gmail.com>
2021-04-02 15:23:35 +0200

    Update __init__.py

--- MRD/__init__.py
+++ MRD/__init__.py
@@ -3,10 +3,13 @@
 # Date: 02/2021
 
 # Be careful with the Global paths
-# rootPathMRD 
-# rootPathSUR 
+# rootPathMRD = "N:\\MRI\\MRDData\\"
+# rootPathSUR = "N:\\MRI\\OriginalData\\"
 #
 # __init__.py
+
+
+# Raw data in KSpace
 from .MRDOI import get_mrd_3d  # Done
 from .MRDOI import open_mrd_file  # Done
 from .MRDOI import open_sur_file  # Done
@@ -16,8 +19,9 @@ from .MRDOI import create_sur_path  # Done
 from .MRDOI import recon_mrd_fse2d  # it's working when there is rtable   (you can export k-space with this function)
 from .MRDOI import recon_corrected_kspace  # Done (kspace reconstruction )
 
+# Apply in KSpace
 from .KSpace import add_motion_artifacts  # Done random every time
-from .KSpace import add_noise_artifacts  # Done random every time
-from .KSpace import hanning_filter  # Done
+from .KSpace import add_noise_artifacts  # Done: Gaussian
+from .KSpace import hanning_filter  # Done 
 from .KSpace import add_high_pass_filter_artifacts # We don't need it
 from .KSpace import add_low_pass_filter_artifacts # We don't need it
